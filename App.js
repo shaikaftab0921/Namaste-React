d1442b1ff4a0fe034cf1c075c5589ffdb8228311
shaikaftab0921 <shaikaftab0921@gmail.com>
2023-07-14 02:04:29 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+const heading=React.createElement('h1',{},'Namaste react ');
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
